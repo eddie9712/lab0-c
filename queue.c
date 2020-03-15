4aa6e82129a11ae7db80d8b2768b913775a21e8d
eddie <qw1562435@gmail.com>
2020-03-15 15:15:47 +0800

    Modify queue.c and queue.h
    1.add tail to the structure queue_t
    2.finish q_new,q_free,q_insert_head....
    3.deal with memory leak
    4.detect dangerous function strcpy and change it

--- queue.c
+++ queue.c
@@ -5,6 +5,9 @@
 #include "harness.h"
 #include "queue.h"
 
+#ifndef strlcpy
+#define strlcpy(dst, src, sz) snprintf((dst), (sz), "%s", (src))
+#endif
 /*
  * Create empty queue.
  * Return NULL if could not allocate space.
@@ -13,7 +16,11 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+    }
     return q;
 }
 
@@ -22,9 +29,19 @@ void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
-    free(q);
+    if (q != NULL) {
+        if (q->head != NULL) {
+            list_ele_t *tmp;
+            while (q->head != NULL) {
+                tmp = q->head;
+                free(tmp->value);  // free the string
+                q->head = tmp->next;
+                free(tmp);
+            }
+        }
+        free(q);
+    }
 }
-
 /*
  * Attempt to insert element at head of queue.
  * Return true if successful.
@@ -34,16 +51,25 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
+    if (q == NULL)
+        return false;
+    list_ele_t *newh;
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL) {
+        free(newh);
+        return false;
+    }
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+    if (q->head == NULL)
+        q->tail = newh;
+    strlcpy(newh->value, s, strlen(s) + 1);  // copy the string to the node
     newh->next = q->head;
     q->head = newh;
+    q->size++;
     return true;
 }
-
 /*
  * Attempt to insert element at tail of queue.
  * Return true if successful.
@@ -53,10 +79,26 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    list_ele_t *newt = malloc(sizeof(list_ele_t));
+    if (q == NULL) {
+        free(newt);
+        return false;
+    }
+    if (newt == NULL) {  // fail to allocate
+        free(newt);
+        return false;
+    }
+    strlcpy(newt->value, s, strlen(s) + 1);
+    if (q->tail == NULL) {
+        newt->next = q->head;
+        q->head = newt;
+    } else {
+        newt->next = q->tail->next;
+        q->tail->next = newt;
+        q->tail = newt;
+    }
+    q->size++;
+    return true;
 }
 
 /*
@@ -69,9 +111,16 @@ bool q_insert_tail(queue_t *q, char *s)
  */
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
-    /* TODO: You need to fix up this code. */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if ((q == NULL) || (q->head == NULL))  // queue is null or empty
+        return false;
+    list_ele_t *ptr = q->head;
+    memmove(sp, q->head->value, bufsize - 1);
+    free(q->head->value);
     q->head = q->head->next;
+    free(ptr);
+    if (q->head == NULL)
+        q->tail = NULL;
+    q->size--;
     return true;
 }
 
@@ -81,10 +130,9 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return 0;
+    if (q == NULL)
+        return 0;
+    return q->size;
 }
 
 /*
@@ -96,8 +144,17 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (q->head != NULL) {
+        list_ele_t *prev = NULL, *cur = q->head, *next = NULL;
+        q->tail = q->head;
+        while (cur != NULL) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        q->head = prev;
+    }
 }
 
 /*
@@ -105,8 +162,4 @@ void q_reverse(queue_t *q)
  * No effect if q is NULL or empty. In addition, if q has only one
  * element, do nothing.
  */
-void q_sort(queue_t *q)
-{
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
-}
+void q_sort(queue_t *q) {}
